57328f669bfe22ff3149f9595ddc46c53ce5d895
schla <schla@LenovoYOGA7i.fritz.box>
2021-07-30 21:29:22 +0200

    created new Class to develop new functionality

---
+++ FirstGitProject/src/package1/DevelopedFunction.java
@@ -0,0 +1,22 @@
+package package1;
+
+/**
+ * contains a new developed function for my little Test Project
+ * @author schla
+ *
+ */
+
+public class DevelopedFunction {
+	
+	/**
+	 * new developed function for TestProject
+	 * @param s word
+	 * @return length of word
+	 */
+	
+	public int newDeveloped(String s) {
+		char[] splitted = s.toCharArray();
+		return splitted.length;
+	}
+
+}
